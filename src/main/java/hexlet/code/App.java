7995672d3d8216d589926c16ead910b0c1f7b4d0
Evgeny Pushkin <seppuku@yandex.ru>
2021-12-11 22:33:41 +0700

    add Even game

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,8 +1,25 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.getName();
+        int id;
+        String name;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        id = sc.nextInt();
+        if (id == 1) {
+            System.out.println("\nWelcome to the Brain Games!");
+            name = Cli.getName();
+        } else if (id == 2) {
+            System.out.println("\nWelcome to the Brain Games!");
+            name = Cli.getName();
+            Even.startEvenGame(name);
+        }
     }
 }
---
+++ src/main/java/hexlet/code/Even.java
@@ -0,0 +1,44 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Even {
+    public static void startEvenGame(String name) {
+        final int range = 100; // range 0..100
+        final int countRepeat = 3;
+
+        boolean breakGame = false;
+        Double randomNumber;
+        int remainder;
+        String answer;
+        String corectAnswer;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
+
+        for (int i = 0; i < countRepeat; i++) {
+            randomNumber = Math.random() * range;
+            System.out.println("Question: " + randomNumber.intValue());
+            answer = sc.nextLine();
+            remainder = randomNumber.intValue() % 2;
+
+            if (remainder == 0) {
+                corectAnswer = "yes";
+            } else {
+                corectAnswer = "no";
+            }
+
+            if (answer.equals(corectAnswer)) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was");
+                System.out.println("'" + corectAnswer + "'.\nLet's try again, " + name + "!");
+                i = countRepeat;
+                breakGame = true;
+            }
+        }
+        if (!breakGame) {
+            System.out.println("Congratulations, " + name + "!");
+        }
+
+    }
+}
